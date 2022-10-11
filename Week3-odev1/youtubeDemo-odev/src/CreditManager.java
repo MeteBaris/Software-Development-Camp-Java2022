121e8af36c15d1d327a10c153b65a311606bea37
Mete Baris <45924151+MeteBaris@users.noreply.github.com>
2022-10-11 15:40:35 +0200

    Add files via upload

---
+++ Week3-odev1/youtubeDemo-odev/src/CreditManager.java
@@ -0,0 +1,16 @@
+public class CreditManager {
+    public void calculate(int creditType){
+        if (creditType == 1){
+
+        }
+        if (creditType == 2){
+
+        }
+
+        System.out.println("Hesaplandi");
+    }
+
+    public void save(){
+        System.out.println("kredi verildi");
+    }
+}
