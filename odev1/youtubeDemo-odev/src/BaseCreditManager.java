bfed95a76487da098a3518bede4d0ae764d48f84
Mete Baris <45924151+MeteBaris@users.noreply.github.com>
2022-10-11 15:32:01 +0200

    Delete odev1/youtubeDemo-odev directory

--- odev1/youtubeDemo-odev/src/BaseCreditManager.java
+++
@@ -1,6 +0,0 @@
-public abstract class BaseCreditManager implements ICreditManager{
-    public abstract void calculate();
-    public void save() {
-        System.out.println("kaydedildi ");
-    }
-}
--- odev1/youtubeDemo-odev/src/Person.java
+++
@@ -1,6 +0,0 @@
-public class Person {
-    public String firstName;
-    public String lastName;
-    public String nationalIdentity;
-
-}
