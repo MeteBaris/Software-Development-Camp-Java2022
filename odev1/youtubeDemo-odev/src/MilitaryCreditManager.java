663a099ce45418b8aba10b423345be3ebdaf1706
Mete Baris <45924151+MeteBaris@users.noreply.github.com>
2022-10-11 15:30:59 +0200

    Add files via upload

---
+++ odev1/youtubeDemo-odev/src/MilitaryCreditManager.java
@@ -0,0 +1,11 @@
+public class MilitaryCreditManager implements ICreditManager{
+    @Override
+    public void calculate() {
+        System.out.println("Asker kredisi hesaplandi");
+    }
+
+    @Override
+    public void save() {
+
+    }
+}
